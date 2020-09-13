8cf54e468abaf5686558c4149a464ac330430454
Dylan Baker <dylang.baker@googlemail.com>
2020-09-13 15:32:42 -0400

    remove property fields

--- models/stg_shopify__order_line.sql
+++ models/stg_shopify__order_line.sql
@@ -27,12 +27,7 @@ renamed as (
         total_discount,
         pre_tax_price,
         fulfillment_status,
-        _fivetran_synced,
-        property_for_shipping_jan_3_rd_2020,
-        property_charge_interval_frequency,
-        property_shipping_interval_unit_type,
-        property_shipping_interval_frequency,
-        property_subscription_id
+        _fivetran_synced
 
     from source
 
