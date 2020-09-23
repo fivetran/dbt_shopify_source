3a8bb8125e3fa91d2ef61841a9fc395fc6d2b298
Dylan Baker <dylang.baker@googlemail.com>
2020-09-23 13:11:36 -0400

    update project to use fivetran_utils

--- models/stg_shopify__product.sql
+++ models/stg_shopify__product.sql
@@ -9,7 +9,7 @@ renamed as (
     select
     
         {{
-            fill_staging_columns(
+            fivetran_utils.fill_staging_columns(
                 source_columns=adapter.get_columns_in_relation(ref('stg_shopify__product_tmp')),
                 staging_columns=get_product_columns()
             )
