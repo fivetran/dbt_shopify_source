1e37f807ff075fc359c79ee551a1f02941900ca7
Martin Guindon <14781590+MartinGuindon@users.noreply.github.com>
2021-03-04 13:57:25 -0500

    Add product variants

--- macros/staging_columns.sql
+++ macros/staging_columns.sql
@@ -167,3 +167,39 @@
 {{ return(columns) }}
 
 {% endmacro %}
+
+{% macro get_product_variant_columns() %}
+
+{% set columns = [
+    {"name": "id", "datatype": dbt_utils.type_numeric(), "alias": "product_variant_id"},
+    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
+    {"name": "created_at", "datatype": dbt_utils.type_timestamp(), "alias": "created_timestamp"},
+    {"name": "updated_at", "datatype": dbt_utils.type_timestamp(), "alias": "updated_timestamp"},
+    {"name": "product_id", "datatype": dbt_utils.type_numeric()},
+    {"name": "inventory_item_id", "datatype": dbt_utils.type_numeric()},
+    {"name": "image_id", "datatype": dbt_utils.type_numeric()},
+    {"name": "title", "datatype": dbt_utils.type_string()},
+    {"name": "price", "datatype": dbt_utils.type_float()},
+    {"name": "sku", "datatype": dbt_utils.type_string()},
+    {"name": "position", "datatype": dbt_utils.type_numeric()},
+    {"name": "inventory_policy", "datatype": dbt_utils.type_string()},
+    {"name": "compare_at_price", "datatype": dbt_utils.type_float()},
+    {"name": "fulfillment_service", "datatype": dbt_utils.type_string()},
+    {"name": "inventory_management", "datatype": dbt_utils.type_string()},
+    {"name": "taxable", "datatype": "boolean", "alias": "is_taxable"},
+    {"name": "barcode", "datatype": dbt_utils.type_string()},
+    {"name": "grams", "datatype": dbt_utils.type_float()},
+    {"name": "inventory_quantity", "datatype": dbt_utils.type_numeric()},
+    {"name": "weight", "datatype": dbt_utils.type_float()},
+    {"name": "weight_unit", "datatype": dbt_utils.type_string()},
+    {"name": "option_1", "datatype": dbt_utils.type_string()},
+    {"name": "option_2", "datatype": dbt_utils.type_string()},
+    {"name": "option_3", "datatype": dbt_utils.type_string()},    
+    {"name": "tax_code", "datatype": dbt_utils.type_string()},
+    {"name": "old_inventory_quantity", "datatype": dbt_utils.type_numeric()},
+    {"name": "requires_shipping", "datatype": "boolean", "alias": "is_requiring_shipping"}
+] %}
+
+{{ return(columns) }}
+
+{% endmacro %}
