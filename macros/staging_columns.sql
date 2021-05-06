3cdaca5c91b0d6a032331a19b95b097b5b0dd3ec
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2021-05-06 15:07:55 -0500

    quote removal

--- macros/staging_columns.sql
+++ macros/staging_columns.sql
@@ -248,3 +248,37 @@
 {{ return(columns) }}
 
 {% endmacro %}
+
+{% macro get_refund_columns() %}
+
+{% set columns = [
+    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
+    {"name": "created_at", "datatype": dbt_utils.type_timestamp()},
+    {"name": "id", "datatype": dbt_utils.type_numeric()},
+    {"name": "note", "datatype": dbt_utils.type_string()},
+    {"name": "order_id", "datatype": dbt_utils.type_numeric()},
+    {"name": "processed_at", "datatype": dbt_utils.type_timestamp()},
+    {"name": "restock", "datatype": "boolean"},
+    {"name": "user_id", "datatype": dbt_utils.type_numeric()}
+] %}
+
+{{ return(columns) }}
+
+{% endmacro %}
+
+{% macro get_order_adjustment_columns() %}
+
+{% set columns = [
+    {"name": "id", "datatype":  dbt_utils.type_numeric()},
+    {"name": "order_id", "datatype":  dbt_utils.type_numeric()},
+    {"name": "refund_id", "datatype":  dbt_utils.type_numeric()},
+    {"name": "amount", "datatype": dbt_utils.type_float()},
+    {"name": "tax_amount", "datatype": dbt_utils.type_float()},
+    {"name": "kind", "datatype": dbt_utils.type_string()},
+    {"name": "reason", "datatype": dbt_utils.type_string()},
+    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()}
+] %}
+
+{{ return(columns) }}
+
+{% endmacro %}
