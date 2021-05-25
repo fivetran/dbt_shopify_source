546882dbeb5d81e5229ce3b7d7b1ab5ceb90b7c5
Dylan Baker <dylan@dbanalytics.co>
2021-05-25 14:48:43 -0400

    union example draft

--- models/stg_shopify__customer.sql
+++ models/stg_shopify__customer.sql
@@ -22,6 +22,10 @@ renamed as (
 
       {% endif %}
 
+      {% if var('union_schemas', none) or var('union_databases', none) %}
+      , _dbt_source_relation as source_relation
+      {% endif %}
+
     from source
 
 )
--- models/tmp/stg_shopify__customer_tmp.sql
+++ models/tmp/stg_shopify__customer_tmp.sql
@@ -1,2 +1,42 @@
-select * 
-from {{ var('customer_source') }}
+{% if var('union_schemas', none) %}
+
+    {% set relations = [] %}
+
+    {% for schema in var('union_schemas') %}
+
+    {% set relation=adapter.get_relation(
+        database=var('shopify_database', target.database),
+        schema=schema,
+        identifier="customer"
+    ) -%}
+
+    {% do relations.append(relation) %}
+
+    {% endfor %}
+
+    {{ dbt_utils.union_relations(relations) }}
+
+{% elif var('union_databases', none) %}
+
+    {% set relations = [] %}
+
+    {% for database in var('union_databases') %}
+
+    {% set relation=adapter.get_relation(
+        database=database,
+        schema=var('shopify_schema', 'shopify'),
+        identifier="customer"
+    ) -%}
+
+    {% do relations.append(relation) %}
+
+    {% endfor %}
+
+    {{ dbt_utils.union_relations(relations) }}
+
+{% else %}
+
+    select * 
+    from {{ var('customer_source') }}
+
+{% endif %}
