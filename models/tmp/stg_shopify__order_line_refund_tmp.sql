e2f5748f983ef210f3b27e89c874601c4b9d25a0
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2023-01-05 16:00:28 -0800

    order line refund work

--- models/tmp/stg_shopify__order_line_refund_tmp.sql
+++ models/tmp/stg_shopify__order_line_refund_tmp.sql
@@ -1,6 +1,15 @@
 --To disable this model, set the shopify__using_order_line_refund variable within your dbt_project.yml file to False.
 {{ config(enabled=var('shopify__using_order_line_refund', True)) }}
 
+{%- set source_relation = adapter.get_relation(
+        database=source('shopify', 'order_line_refund').database,
+        schema=source('shopify', 'order_line_refund').schema,
+        identifier=source('shopify', 'order_line_refund').name) -%}
+
+{% set table_exists=source_relation is not none  %}
+
+{% if table_exists %}
+
 {{
     fivetran_utils.union_data(
         table_identifier='order_line_refund', 
@@ -13,3 +22,21 @@
         union_database_variable='shopify_union_databases'
     )
 }}
+
+{% else %}
+
+select
+    cast(null as {{ dbt.type_timestamp() }}) as _fivetran_synced,
+    cast(null as {{ dbt.type_numeric() }}) as id,
+    cast(null as {{ dbt.type_numeric() }}) as location_id,
+    cast(null as {{ dbt.type_numeric() }}) as order_line_id,
+    cast(null as {{ dbt.type_numeric() }}) as subtotal,
+    cast(null as {{ dbt.type_string() }}) as subtotal_set,
+    cast(null as {{ dbt.type_numeric() }}) as total_tax,
+    cast(null as {{ dbt.type_string() }}) as total_tax_set,
+    cast(null as {{ dbt.type_float() }}) as quantity,
+    cast(null as {{ dbt.type_numeric() }}) as refund_id,
+    cast(null as {{ dbt.type_string() }}) as restock_type,
+    cast(null as {{ dbt.type_string() }}) as _dbt_source_relation
+
+{% endif %}
