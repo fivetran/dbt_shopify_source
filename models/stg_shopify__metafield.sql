8eb3ed80ab39bd028cfea5544f6a69d65f173bd6
Zach Crookston <116017993+ZCrookston@users.noreply.github.com>
2023-04-12 15:41:01 -0600

    Update stg_shopify__metafield.sql
    resolves an ambiguous column error when the namespace or key fields have inconsistent casing (capital vs lower case names).

--- models/stg_shopify__metafield.sql
+++ models/stg_shopify__metafield.sql
@@ -37,7 +37,7 @@ final as (
         {{ dbt_date.convert_timezone(column='cast(created_at as ' ~ dbt.type_timestamp() ~ ')', target_tz=var('shopify_timezone', "UTC"), source_tz="UTC") }} as created_at,
         {{ dbt_date.convert_timezone(column='cast(updated_at as ' ~ dbt.type_timestamp() ~ ')', target_tz=var('shopify_timezone', "UTC"), source_tz="UTC") }} as updated_at,
         {{ dbt_date.convert_timezone(column='cast(_fivetran_synced as ' ~ dbt.type_timestamp() ~ ')', target_tz=var('shopify_timezone', "UTC"), source_tz="UTC") }} as _fivetran_synced,
-        {{ dbt.concat(["namespace","'_'","key"]) }} as metafield_reference,
+        lower({{ dbt.concat(["namespace","'_'","key"]) }}) as metafield_reference,
         row_number() over (partition by id order by updated_at desc) = 1 as is_most_recent_record,
         source_relation
         
