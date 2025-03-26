e2fcd5978d9426a384e5b0454b590615ccffda61
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2025-03-26 12:25:46 -0700

    removed trino

--- macros/fivetran_date_macros/fivetran_convert_timezone.sql
+++ macros/fivetran_date_macros/fivetran_convert_timezone.sql
@@ -35,7 +35,3 @@ from_utc_timestamp(
         '{{ target_tz }}'
         )
 {%- endmacro -%}
-
-{%- macro trino__fivetran_convert_timezone(column, target_tz, source_tz) -%}
-    cast((at_timezone(with_timezone(cast({{ column }} as {{ dbt.type_timestamp() }}), '{{ source_tz }}'), '{{ target_tz }}')) as {{ dbt.type_timestamp() }})
-{%- endmacro -%}
